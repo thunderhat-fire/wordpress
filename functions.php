dbd21baccd38c28b1c096f836be6c79fc826149a
ross <ross@Olivers-MacBook-Air.local>
2019-11-13 16:57:35 +0000

    changed custom posts type in menu, and added new pages

--- functions.php
+++ functions.php
@@ -13,3 +13,40 @@ function filter_example($title) {
 	return 'Hooked: '.$title;
 }
 
+function create_custom_post_type_case_study(){
+	$labels = array(
+		'name' => 'Case Study',
+		'singular_name' => 'Event',
+		'add_new' => 'Add New',
+		'add_new_item' => 'Add New Event',
+		'edit_item' => 'Edit Event',
+		'new_item' => 'New Event',
+		'view_item' => 'View Event',
+		'search_items' => 'Search Events',
+		'not_found' => 'No events found',
+		'not_found_in_trash' => 'No events found in Trash',
+		'parent_item_colon' => '',
+	);
+	
+	$args = array(
+
+		'label' => __('Case Study'),
+		'labels' => $labels, // from array above
+		'public' => true,
+		'can_export' => true,
+		'show_ui' => true,
+		'_builtin' => false,
+		'capability_type' => 'post',
+		'menu_icon' => 'dashicons-calendar', // from this list
+		'hierarchical' => false,
+		'rewrite' => array( "slug" => "meetups" ), // defines URL base
+		'supports'=> array('title', 'thumbnail', 'editor', 'excerpt'),
+		'show_in_nav_menus' => true,
+		'taxonomies' => array( 'event_category', 'post_tag') // own categories
+	);
+
+
+	register_post_type('case-study', $args); // used as internal identifier
+}
+
+add_action('init', 'create_custom_post_type_case_study');
